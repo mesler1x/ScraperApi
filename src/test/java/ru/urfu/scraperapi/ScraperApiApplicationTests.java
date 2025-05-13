a5ed559d1323cd810108b5714714a03eb2b6fb00
Roman Mesler <meslerdev@gmail.com>
2025-05-13 14:40:27 +0500

    first commit

---
+++ src/test/java/ru/urfu/scraperapi/ScraperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.scraperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
