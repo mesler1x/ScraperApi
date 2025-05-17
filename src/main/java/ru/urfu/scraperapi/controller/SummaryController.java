825dfc121d30116b21b341561594bacd93df2564
Roman Mesler <meslerdev@gmail.com>
2025-05-17 13:15:06 +0500

    set up spring docs

--- src/main/java/ru/urfu/scraperapi/controller/SummaryController.java
+++ src/main/java/ru/urfu/scraperapi/controller/SummaryController.java
@@ -1,5 +1,7 @@
 package ru.urfu.scraperapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.urfu.scraperapi.dto.SummaryResponse;
@@ -11,20 +13,25 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/summary")
+@Tag(name = "Сервис саммари", description = "Получение саммари по публикациям, собраных скрапером")
 public class SummaryController {
     private final SummaryService summaryService;
 
+
     @PostMapping("/{publicationId}")
+    @Operation(summary = "Получить саммари по id публикации")
     public SummaryResponse getSummaryByPublicationId(@PathVariable("publicationId") UUID publicationId) {
         return summaryService.getSummaryByPublicationId(publicationId);
     }
 
     @PostMapping("/{summaryId}/regenerate")
+    @Operation(summary = "Перегенерировать саммари по id саммари")
     public SummaryResponse regenerateSummaryByIdAndGet(@PathVariable("summaryId") UUID summaryId) {
         return summaryService.regenerateSummaryByIdAndGet(summaryId);
     }
 
     @GetMapping
+    @Operation(summary = "Получить все саммари по публикациям")
     public List<SummaryResponse> getAllSummaries() {
         return summaryService.getAllSummaries();
     }
